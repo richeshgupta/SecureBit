628de1c0d8fb9ee4458c4b6662fcfc9e8d735568
Richesh Gupta <rgrichesh45@gmail.com>
2020-08-02 16:30:48 +0530

    Hex Conversion complete

--- aes-py/state_array.py
+++ aes-py/state_array.py
@@ -1,14 +1,20 @@
 # trying for 128 bit key only
 from const_aes import *
+import numpy as np
 
 state_array = [[0 for x in range(4)] for x in range(4)]
 
 def hexconversion(text):
-    hex_code  = ""
-    for i in text:
-        hex_code = hex_code + hex(ord(i))[2:]
-    
-    return hex_code
+    hex_code  = []
+    temp=[]
+    print("text : ",text)
+    for i in range(len(text)):
+       
+        temp.append([hex(ord(text[i]))[2:]])
+        temp[i] = int(temp[i][0])
+        print(temp)
+
+    return temp
 
 def padd(text,key):
     present_len = len(text)   
@@ -17,19 +23,21 @@ def padd(text,key):
     elif present_len<16:
         null_add = 16 - present_len
         text  = text + ( (null_add)*"0")
+    return text
 
-def text2matrix(text):
-    matrix = []
-    return text[0:]
+
+def text2matrix(cipher_array):
+    return "TO be done"
+    
 
 
 def interact():
     text = input("Enter Text to encrypt : ")
     key = input("Enter Key : ")
     text = padd(text,key)
-    
-    
-    print(text2matrix(hexconversion(text)))
+    cipher_array = hexconversion(text)
+    cypher_matrix = text2matrix(cipher_array)
+    # print(matrix)
 
 if __name__ == "__main__":
     interact() 
