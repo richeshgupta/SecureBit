e85b373c9f4106b2543265741fa5dc8ab086d34e
Richesh Gupta <rgrichesh45@gmail.com>
2021-01-31 15:54:01 +0530

    Updated D formula

--- encryption/rsa.py
+++ encryption/rsa.py
@@ -42,7 +42,7 @@ def PrimeGenerator(length=23):
         if(length<=0):
             return choice(primes)
             
-print(PrimeGenerator())
+
 
 def Multiply(p,q):
     return p*q
@@ -56,6 +56,40 @@ def choose_e(number):
         if( is_prime(i)==True and gcd(i,number)==1):
             return i
 
+def choose_d(a, m):
+    m0 = m
+    y = 0
+    x = 1
+ 
+    if (m == 1):
+        return 0
+ 
+    while (a > 1):
+ 
+        # q is quotient
+        q = a // m
+ 
+        t = m
+ 
+        # m is remainder now, process
+        # same as Euclid's algo
+        # a = e (exponent) and m -> phi_n
+        m = a % m
+        a = t
+        t = y
+ 
+        # Update x and y
+        y = x - q * y
+        x = t
+ 
+    # Make x positive
+    if (x < 0):
+        x = x + m0
+ 
+    return x
+    
+
+
 def main():
     p = -1
     q = -1
@@ -66,10 +100,12 @@ def main():
     n = Multiply(p,q)
     phi_n = phi(p,q)
     e=choose_e(phi_n)
-    
-    
+    seed()
+    # k = randint(2,10)
     k = 2
-    d = (1+(k*phi_n))/e
+    d = choose_d(e,phi_n)
+    # d = (1 + (k*phi_n))/e
+    # d = int(d)
     print("p : ",p,"q : ",q,"n : ",n,'phin_n',phi_n,"e:",e,"d : ",d)
     return e,n,d,n
 
